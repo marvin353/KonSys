c142d6af0e50c551a4316c1fc10b9a827543c125
Marvin Woller <marvinwoller@yahoo.de>
2022-11-23 00:27:19 +0100

    final

--- WebAppPrediction/js/index.js
+++ WebAppPrediction/js/index.js
@@ -1,176 +1,344 @@
 //------------------------------------ Global Variables and Init Functions ------------------------------------\\
 
+var ctw_value = "table"; //"car", "walk"
+var defaultTags = {};
+
+var alpha = 0;
+var beta = 0;
+var gamma = 0;
+var x = 0;
+var y = 0;
+var z = 0;
+var x0 = 0;
+var y0 = 0;
+var z0 = 0;
+
+cwt();
 //------------------------------------ HTML Element Event Listeners ------------------------------------\\
 
 document.getElementById("requestBtn").addEventListener("click", permission);
 
-/*document
-  .getElementById("DeviceOrientation_Switch")
-  .addEventListener("change", device_orientation_switched);
-document
-  .getElementById("DeviceMotion_Switch")
-  .addEventListener("change", device_motion_switched);*/
+document.getElementById("Upload_Switch").onchange = function () {
+  if (this.checked) {
+    start_prediction();
+  } else {
+    stop_prediction();
+  }
+};
 
-var measure_values = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+//------------------------------------ Mobile Meta Data and Sensors ------------------------------------\\
 
-function handleMotionEvent(event) {
-    measure_values[0] = event.rotationRate.alpha;
-    measure_values[1] = event.rotationRate.beta;
-    measure_values[2] = event.rotationRate.gamma;
-    measure_values[3] = event.accelerationIncludingGravity.x;
-    measure_values[4] = event.accelerationIncludingGravity.y;
-    measure_values[5] = event.accelerationIncludingGravity.z;
-    measure_values[6] = event.acceleration.x;
-    measure_values[7] = event.acceleration.y;
-    measure_values[8] = event.acceleration.z;
+const MobileDetect = require("mobile-detect");
+const mobile = new MobileDetect(window.navigator.userAgent);
 
-    //var text = document.getElementById("text");
-    //text.innerText = "Alpha:" + a + ", Beta:" + b + ", Gamma:" + g;
+if (mobile.mobile()) {
+  defaultTags.mobile = mobile.mobile();
 }
 
-function switched() {
-    var checkBox = document.getElementById("myCheck");
-    if (checkBox.checked == true) {
-        if (window.DeviceOrientationEvent) {
-            window.addEventListener("deviceorientation", handleMotionEvent, false);
-        }
-    } else {
-        if (window.DeviceOrientationEvent) {
-            window.removeEventListener("deviceorientation", handleMotionEvent, false);
-        }
+if (mobile.userAgent()) {
+  defaultTags.browser = mobile.userAgent();
+}
+
+const sensors = {
+  devicemotion: {
+    listener: function (/** @type {DeviceMotionEvent} */ evt) {
+      if (evt.acceleration.x === null) return;
+
+      alpha = evt.rotationRate.alpha;
+      beta = evt.rotationRate.beta;
+      gamma = evt.rotationRate.gamma;
+      x = evt.accelerationIncludingGravity.x;
+      y = evt.accelerationIncludingGravity.y;
+      z = evt.accelerationIncludingGravity.z;
+      x0 = evt.acceleration.x;
+      y0 = evt.acceleration.y;
+      z0 = evt.acceleration.z;
+
+      document.getElementById("DeviceMotion_Text").innerText = alpha;
     }
+  }
+};
+
+//------------------------------------ Data Collection ------------------------------------\\
+
+/*const datasetCollector = require("edge-ml").datasetCollector;
+ */
+async function start_prediction() {
+  for (const [sensor, fun] of Object.entries(sensors)) {
+    window.addEventListener(sensor, fun.listener, true);
+  }
+}
+
+async function stop_prediction() {
+  for (const [sensor, fun] of Object.entries(sensors)) {
+    window.removeEventListener(sensor, fun.listener, true);
+  }
 }
 
 const { Predictor } = require("edge-ml");
-const { score } = require("./EdgeMLDecTree_javascript");
+const { score } = require("./EdgeMLRndForrest_javascript");
+
 const p = new Predictor(
-    (input) => score(input),
-    ["alpha", "beta", "gamma", "x0", "y0", "z0", "x", "y", "z"],
-    100,
-    [
-        "62d96e3bc11866001371c055",
-        "62d96e3bc11866001371c056",
-        "62d96e3bc11866001371c057"
-    ]
+  (input) => score(input),
+  ["alpha", "beta", "gamma", "x0", "y0", "z0", "x", "y", "z"], // sensors
+  100, // window size
+  ["car", "walk", "table"], // labels
+  {
+    scale: [
+      19.352500000000003,
+      0.1525,
+      0.19352500000000003,
+      1.0,
+      0.7064562270234316,
+      0.52741242,
+      0.9438882405513817,
+      1.5825,
+      2.37,
+      2.1375,
+      36.16,
+      0.36375,
+      0.3616,
+      1.0,
+      1.04433588992871,
+      1.1388326325000002,
+      1.2238741396992037,
+      2.2775,
+      2.6625,
+      2.1550000000000002,
+      15.614999999999998,
+      0.16375,
+      0.15614999999999998,
+      1.0,
+      0.9522026620469596,
+      0.9393641949999999,
+      0.930195123222176,
+      2.4475,
+      3.115,
+      2.9175,
+      94.87499999999999,
+      0.9125,
+      0.94875,
+      1.0,
+      0.8694905491289409,
+      0.7913674425,
+      1.3939325238160039,
+      0.6875,
+      2.81,
+      2.53,
+      367.3025,
+      3.7075,
+      3.673025,
+      1.0,
+      1.2069170720707714,
+      1.5115038099999998,
+      4.226379522931828,
+      3.3000000000000003,
+      6.34,
+      4.2875000000000005,
+      131.995,
+      1.4949999999999992,
+      1.3199499999999986,
+      1.0,
+      0.9151081334547534,
+      0.8686545299999999,
+      1.1058347861194981,
+      5.172499999999999,
+      1.2975000000000012,
+      1.25,
+      26.58,
+      0.24625000000000002,
+      0.2657999999999999,
+      1.0,
+      22.274688915836435,
+      500.30070076000004,
+      23.10043066475372,
+      47.7025,
+      54.977500000000006,
+      43.857499999999995,
+      65.65,
+      0.83375,
+      0.6565000000000001,
+      1.0,
+      20.283448655756075,
+      415.11548980750007,
+      20.88635433524221,
+      42.7375,
+      51.065,
+      45.57,
+      130.97249999999997,
+      1.4725,
+      1.309725,
+      1.0,
+      19.780589889177563,
+      394.26538844250007,
+      21.28313307048203,
+      33.940000000000005,
+      48.190000000000005,
+      39.205
+    ],
+    center: [
+      -0.29000000000000004,
+      0.0,
+      -0.0029,
+      100.0,
+      0.17371027312426385,
+      0.030175414999999997,
+      0.2565905394910363,
+      0.315,
+      0.655,
+      -0.335,
+      1.37,
+      0.01,
+      0.0137,
+      100.0,
+      0.1924433665447708,
+      0.037034495,
+      0.3838714434904469,
+      0.595,
+      0.84,
+      -0.24,
+      4.68,
+      0.05,
+      0.046799999999999994,
+      100.0,
+      0.34450461217678446,
+      0.11868367,
+      0.40548114206677155,
+      0.855,
+      1.11,
+      -0.965,
+      -43.21500000000001,
+      -0.4125,
+      -0.43215000000000015,
+      100.0,
+      0.18272149088133746,
+      0.033387494999999996,
+      0.6098443361905149,
+      0.01,
+      1.15,
+      -0.87,
+      -324.895,
+      -3.2375000000000003,
+      -3.24895,
+      100.0,
+      0.1989999373308819,
+      0.039600979999999994,
+      3.597622664900909,
+      -2.12,
+      4.035,
+      -3.68,
+      -905.02,
+      -9.03,
+      -9.0502,
+      100.0,
+      0.34334307992483915,
+      0.11788465999999996,
+      9.058789597519237,
+      -8.135000000000002,
+      9.79,
+      -9.79,
+      0.08500000000000002,
+      0.0,
+      0.0008500000000000003,
+      100.0,
+      1.0230625472400707,
+      1.046710215,
+      1.070198303346283,
+      2.625,
+      3.035,
+      -2.57,
+      0.13999999999999999,
+      0.005,
+      0.0013999999999999998,
+      100.0,
+      1.4064848052855168,
+      1.9781996549999998,
+      1.7914784939784538,
+      3.4450000000000003,
+      5.02,
+      -3.485,
+      -0.48499999999999993,
+      0.0,
+      -0.004849999999999999,
+      100.0,
+      0.5045184004988037,
+      0.25454564,
+      1.2410788238416617,
+      0.41,
+      2.95,
+      -0.645
+    ],
+    name: "RobustScaler"
+  }, // scaler
+  {
+    // other options
+    windowingMode: "sample"
+  }
 );
 
-do_prediction();
+setInterval(() => {
+  p.addDatapoint("alpha", alpha);
+  p.addDatapoint("beta", beta);
+  p.addDatapoint("gamma", gamma);
+  p.addDatapoint("x0", x0);
+  p.addDatapoint("y0", y0);
+  p.addDatapoint("z0", z0);
+  p.addDatapoint("x", x);
+  p.addDatapoint("y", y);
+  p.addDatapoint("z", z);
 
-function do_prediction() {
-    setInterval(() => {
-        p.addDatapoint("alpha", measure_values[0]);
-        p.addDatapoint("beta", measure_values[1]);
-        p.addDatapoint("gamma", measure_values[2]);
-        p.addDatapoint("x0", measure_values[3]);
-        p.addDatapoint("y0", measure_values[4]);
-        p.addDatapoint("z0", measure_values[5]);
-        p.addDatapoint("x", measure_values[6]);
-        p.addDatapoint("y", measure_values[7]);
-        p.addDatapoint("z", measure_values[8]);
-
-        p.predict()
-            .then((x) => x)
-            .catch((e) => console.log(e.message));
-    }, 250);
-}
-
-async function store_acc_measurements(event) {
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
-    acc_z.push(z);
-    acc_y.push(y);
-}
-
-function predict(measurement) {
-    console.log(RandomForestClassifier(measurement));
-}
-
-var intervalId = window.setInterval(function () {
-    if (acc_z.length > 2) {
-        var z_mean = getMean(acc_z);
-        var y_std = getStandardDeviation(acc_y);
-        acc_z = [];
-        acc_y = [];
-        var clf = new RandomForestClassifier();
-        var prediction = clf.predict([y_std, z_mean]);
-        if (prediction == 2) {
-            document.getElementById("status").innerHTML =
-                "Sie Joggen, bravo! &#127939";
-        } else if (prediction == 1) {
-            document.getElementById("status").innerHTML =
-                "Achtung: Sie gehen, da geht mehr &#57857";
-        } else if (prediction == 0) {
-            document.getElementById("status").innerHTML =
-                "Setzen Sie sich in Bewegung!";
-        } else {
-            document.getElementById("status").innerHTML = "";
-        }
-    }
-}, 2000);
-
-function controlActivity() {
-    var checked = document.getElementById("switch2").checked;
-
-    if (checked) {
-        console.log(typeof window.DeviceMotionEvent);
-        if (
-            typeof window.DeviceMotionEvent !== undefined &&
-            typeof window.DeviceMotionEvent.requestPermission === "function"
-        ) {
-            window.DeviceMotionEvent.requestPermission()
-                .then((response) => {
-                    if (response == "granted") {
-                        window.addEventListener(
-                            "devicemotion",
-                            store_acc_measurements,
-                            false
-                        );
-                    }
-                })
-                .catch(console.error);
-        }
-    }
-}
+  p.predict()
+    .then((x) => x)
+    .catch((e) => console.log(e.message));
+}, 250);
 
 //------------------------------------ Ask for Permissions (iOS) ------------------------------------\\
 
 function permission() {
-    console.log("Request for permissions");
+  console.log("Request for permissions");
 
-    if (
-        typeof DeviceMotionEvent !== "undefined" &&
-        typeof DeviceMotionEvent.requestPermission === "function"
-    ) {
-        DeviceMotionEvent.requestPermission()
-            .then((response) => {})
-            .catch(console.error);
-    } else {
-        alert("DeviceMotionEvent is not defined");
-    }
+  if (
+    typeof DeviceMotionEvent !== "undefined" &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    DeviceMotionEvent.requestPermission()
+      .then((response) => {})
+      .catch(console.error);
+  } else {
+    alert("DeviceMotionEvent is not defined");
+  }
 
-    if (
-        typeof DeviceOrientationEvent !== "undefined" &&
-        typeof DeviceOrientationEvent.requestPermission === "function"
-    ) {
-        DeviceOrientationEvent.requestPermission()
-            .then((response) => {})
-            .catch(console.error);
-    } else {
-        alert("DeviceOrientationEvent is not defined");
-    }
+  if (
+    typeof DeviceOrientationEvent !== "undefined" &&
+    typeof DeviceOrientationEvent.requestPermission === "function"
+  ) {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {})
+      .catch(console.error);
+  } else {
+    alert("DeviceOrientationEvent is not defined");
+  }
 }
 
 //------------------------------------ Helper Functions ------------------------------------\\
 
-function getStandardDeviation(array) {
-    var n = array.length;
-    var mean = array.reduce((a, b) => a + b) / n;
-    return Math.sqrt(
-        array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
-    );
-}
+function cwt() {
 
-function getMean(array) {
-    var n = array.length;
-    return array.reduce((a, b) => a + b) / n;
+  var prediction_label = document.getElementById("prediction");
+
+  switch (ctw_value) {
+    case "car":
+      prediction_label.innerText = "Car";
+      break;
+    case "table":
+      //tableBtn.style.background = "#00cc00";
+      prediction_label.innerText = "Table";
+      break;
+    case "walk":
+      prediction_label.innerText = "Walk";
+      //walkBtn.style.background = "#00cc00";
+      break;
+    default:
+      break;
+  }
 }
--- Webapp/js/index.js
+++ Webapp/js/index.js
@@ -6,34 +6,29 @@ var defaultTags = {};
 cwt();
 //------------------------------------ HTML Element Event Listeners ------------------------------------\\
 
+
 document.getElementById("requestBtn").addEventListener("click", permission);
 
 document.getElementById("tableBtn").addEventListener("click", () => {
-    ctw_value = "table";
-    cwt();
+  ctw_value = "table";
+  cwt();
 });
 document.getElementById("carBtn").addEventListener("click", () => {
-    ctw_value = "car";
-    cwt();
+  ctw_value = "car";
+  cwt();
 });
 document.getElementById("walkBtn").addEventListener("click", () => {
-    ctw_value = "walk";
-    cwt();
+  ctw_value = "walk";
+  cwt();
 });
 
 document.getElementById("Upload_Switch").onchange = function () {
-    if (this.checked) {
-        start_recording();
-    } else {
-        stop_recording();
-    }
+  if (this.checked) {
+    start_recording();
+  } else {
+    stop_recording();
+  }
 };
-/*document
-  .getElementById("DeviceOrientation_Switch")
-  .addEventListener("change", device_orientation_switched);
-document
-  .getElementById("DeviceMotion_Switch")
-  .addEventListener("change", device_motion_switched);*/
 
 //------------------------------------ Mobile Meta Data and Sensors ------------------------------------\\
 
@@ -41,134 +36,137 @@ const MobileDetect = require("mobile-detect");
 const mobile = new MobileDetect(window.navigator.userAgent);
 
 if (mobile.mobile()) {
-    defaultTags.mobile = mobile.mobile();
+  defaultTags.mobile = mobile.mobile();
 }
 
 if (mobile.userAgent()) {
-    defaultTags.browser = mobile.userAgent();
+  defaultTags.browser = mobile.userAgent();
 }
 
 const sensors = {
-    deviceorientation: {
-        listener: function (/** @type {DeviceOrientationEvent} */ evt) {
-            if (evt.alpha === null) return;
-            record(
-                evt.type,
-                {
-                    alpha: evt.alpha,
-                    beta: evt.beta,
-                    gamma: evt.gamma
-                },
-                evt.timeStamp + performance.timing.navigationStart
-            );
-        }
-    },
-    devicemotion: {
-        listener: function (/** @type {DeviceMotionEvent} */ evt) {
-            if (evt.acceleration.x === null) return;
-            record(
-                evt.type,
-                {
-                    x0: evt.acceleration.x,
-                    y0: evt.acceleration.y,
-                    z0: evt.acceleration.z,
-                    x: evt.accelerationIncludingGravity.x,
-                    y: evt.accelerationIncludingGravity.y,
-                    z: evt.accelerationIncludingGravity.z,
-                    alpha: evt.rotationRate.alpha,
-                    beta: evt.rotationRate.beta,
-                    gamma: evt.rotationRate.gamma
-                },
-                evt.timeStamp + performance.timing.navigationStart
-            );
-        }
+  deviceorientation: {
+    listener: function (/** @type {DeviceOrientationEvent} */ evt) {
+      if (evt.alpha === null) return;
+      record(
+        evt.type,
+        {
+          alpha: evt.alpha,
+          beta: evt.beta,
+          gamma: evt.gamma
+        },
+        evt.timeStamp + performance.timing.navigationStart
+      );
     }
-};
+  },
+  devicemotion: {
+    listener: function (/** @type {DeviceMotionEvent} */ evt) {
+      if (evt.acceleration.x === null) return;
 
+      record(
+        evt.type,
+        {
+          x0: evt.acceleration.x,
+          y0: evt.acceleration.y,
+          z0: evt.acceleration.z,
+          x: evt.accelerationIncludingGravity.x,
+          y: evt.accelerationIncludingGravity.y,
+          z: evt.accelerationIncludingGravity.z,
+          alpha: evt.rotationRate.alpha,
+          beta: evt.rotationRate.beta,
+          gamma: evt.rotationRate.gamma
+        },
+        evt.timeStamp + performance.timing.navigationStart
+      );
+    }
+  }
+};
 
 //------------------------------------ Data Collection ------------------------------------\\
 
 const datasetCollector = require("edge-ml").datasetCollector;
 
 async function start_recording() {
-    for (const [sensor, fun] of Object.entries(sensors)) {
-        fun.collector = await datasetCollector(
-            "https://app.edge-ml.org", // Backend-URL
-            "zfOAqoW+L7b/NzeoMPtdqqn8jVkk5G9Iy8dzT27eCEnm+GiYlHwd1BMz6mcLJPZfupbs0n75xttwqwsCOceJig==", // API-Key
-            sensor, // Name for the dataset
-            false, // False to provide own timestamps
-            Object.assign({ participantId: "UnknownID" }, defaultTags), // Metadata: {} to omit
-            "KonSysLabels_" + ctw_value // Labeling and label for the whole dataset. Format: {labeling}_{label}
-        );
-        window.addEventListener(sensor, fun.listener, true);
-    }
+  for (const [sensor, fun] of Object.entries(sensors)) {
+    fun.collector = await datasetCollector(
+      "https://app.edge-ml.org", // Backend-URL
+      "UeeUAy9qKbgxRiLjRkn5JSqImW3zQ9JmkcCRgUJigoYQ7D8NpShj6F1sh3WeZG0VisWZMNKJsYy6/YSWLRMaFQ==", // API-Key
+      sensor, // Name for the dataset
+      false, // False to provide own timestamps
+
+      // These two parameters can be omitted
+      Object.assign({ participantId: "UnknownID" }, defaultTags), // Metadata: {} to omit
+      "KonSysLabels_" + ctw_value // Labeling and label for the whole dataset. Format: {labeling}_{label}
+    );
+
+    window.addEventListener(sensor, fun.listener, true);
+  }
 }
 
 async function stop_recording() {
-    for (const [sensor, fun] of Object.entries(sensors)) {
-        window.removeEventListener(sensor, fun.listener, true);
-        await fun.collector.onComplete();
-    }
+  for (const [sensor, fun] of Object.entries(sensors)) {
+    window.removeEventListener(sensor, fun.listener, true);
+    await fun.collector.onComplete();
+  }
 }
 
 function record(eventtype, fields, eventtime) {
-    // time at which the event happend
-    for (const [key, value] of Object.entries(fields)) {
-        sensors[eventtype].collector.addDataPoint(eventtime, key, value);
-    }
+  // time at which the event happend
+  for (const [key, value] of Object.entries(fields)) {
+    sensors[eventtype].collector.addDataPoint(eventtime, key, value);
+  }
 }
 
 
 //------------------------------------ Ask for Permissions (iOS) ------------------------------------\\
 
 function permission() {
-    console.log("Request for permissions");
+  console.log("Request for permissions");
 
-    if (
-        typeof DeviceMotionEvent !== "undefined" &&
-        typeof DeviceMotionEvent.requestPermission === "function"
-    ) {
-        DeviceMotionEvent.requestPermission()
-            .then((response) => {})
-            .catch(console.error);
-    } else {
-        alert("DeviceMotionEvent is not defined");
-    }
+  if (
+    typeof DeviceMotionEvent !== "undefined" &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    DeviceMotionEvent.requestPermission()
+      .then((response) => {})
+      .catch(console.error);
+  } else {
+    alert("DeviceMotionEvent is not defined");
+  }
 
-    if (
-        typeof DeviceOrientationEvent !== "undefined" &&
-        typeof DeviceOrientationEvent.requestPermission === "function"
-    ) {
-        DeviceOrientationEvent.requestPermission()
-            .then((response) => {})
-            .catch(console.error);
-    } else {
-        alert("DeviceOrientationEvent is not defined");
-    }
+  if (
+    typeof DeviceOrientationEvent !== "undefined" &&
+    typeof DeviceOrientationEvent.requestPermission === "function"
+  ) {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {})
+      .catch(console.error);
+  } else {
+    alert("DeviceOrientationEvent is not defined");
+  }
 }
 
 //------------------------------------ Helper Functions ------------------------------------\\
 
 function cwt() {
-    var tableBtn = document.getElementById("tableBtn");
-    var walkBtn = document.getElementById("walkBtn");
-    var carBtn = document.getElementById("carBtn");
+  var tableBtn = document.getElementById("tableBtn");
+  var walkBtn = document.getElementById("walkBtn");
+  var carBtn = document.getElementById("carBtn");
 
-    tableBtn.style.background = "#aaaaaa";
-    carBtn.style.background = "#aaaaaa";
-    walkBtn.style.background = "#aaaaaa";
+  tableBtn.style.background = "#aaaaaa";
+  carBtn.style.background = "#aaaaaa";
+  walkBtn.style.background = "#aaaaaa";
 
-    switch (ctw_value) {
-        case "car":
-            carBtn.style.background = "#00cc00";
-            break;
-        case "table":
-            tableBtn.style.background = "#00cc00";
-            break;
-        case "walk":
-            walkBtn.style.background = "#00cc00";
-            break;
-        default:
-            break;
-    }
+  switch (ctw_value) {
+    case "car":
+      carBtn.style.background = "#00cc00";
+      break;
+    case "table":
+      tableBtn.style.background = "#00cc00";
+      break;
+    case "walk":
+      walkBtn.style.background = "#00cc00";
+      break;
+    default:
+      break;
+  }
 }
